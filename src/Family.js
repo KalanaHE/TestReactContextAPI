6a564e70a348185b7a2f0d54d082fa9b3dfc268b
Kalana Hettiarachchi <gimankalana@gmail.com>
2020-09-12 20:02:56 +0530

    init

---
+++ src/Family.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Person from "./Person";
+const Family = (props) => {
+  return (
+    <div>
+      <Person name={props.name} />
+    </div>
+  );
+};
+
+export default Family;
