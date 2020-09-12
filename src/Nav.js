b577379b1845861211769a5055d88dd4499b0404
Kalana Hettiarachchi <gimankalana@gmail.com>
2020-09-12 22:00:48 +0530

    test react context api done

--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,53 @@
+// import React, { useContext } from "react";
+// import { MovieContext } from "./MovieContext";
+// function Nav() {
+//   const [movies, setMovies] = useContext(MovieContext);
+//   return (
+//     <div>
+//       <h3>KalanaHE</h3>
+//       <p>List of movies: {movies.length}</p>
+//     </div>
+//   );
+// }
+
+// export default Nav;
+
 import React, { useContext } from "react";
 import { MovieContext } from "./MovieContext";
-function Nav() {
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const Nav = () => {
   const [movies, setMovies] = useContext(MovieContext);
+  const classes = useStyles();
+
   return (
-    <div>
-      <h3>KalanaHE</h3>
-      <p>List of movies: {movies.length}</p>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Movies
+          </Typography>
+          <Button color="inherit">Number of movies: {movies.length}</Button>
+        </Toolbar>
+      </AppBar>
     </div>
   );
-}
+};
 
 export default Nav;
